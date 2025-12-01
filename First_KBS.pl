a58e2f9f486c02ec3775129152e1f60fe3e57464
Manorooban <manojmanorooban@gmail.com>
2025-12-01 23:37:15 +0530

    First Practical in Prolog Practice

---
+++ First_KBS.pl
@@ -0,0 +1,16 @@
+fruit(mango).
+parrot(eagle).
+food(biriyani).
+car(rolce_royce).
+x(10).
+y(25).
+animal(dog).
+banana(fruit).
+banama(island).
+weapon(gun).
+hardware(keyboard).
+andriod_phone(samsung).
+student(john).
+black(elephant).
+vehicle(car).
+
